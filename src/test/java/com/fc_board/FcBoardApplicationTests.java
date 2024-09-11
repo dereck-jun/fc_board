5ec255fd11ceb9c7a5a36a049b66644f7d7e5f81
dereck-jun <yjpark0805@naver.com>
2024-09-11 16:22:47 +0900

    Initial commit

---
+++ src/test/java/com/fc_board/FcBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
